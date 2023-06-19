3c1e032d860b54c52da68c8cddcee9d2cdf062e6
Moises <moiseswtf123@gmail.com>
2023-06-19 10:37:24 -0500

    preparando paginas para cotenido

--- Dasboard/src/App.jsx
+++ Dasboard/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import styled from "styled-components";
+import { MyRoutes } from "./routers/routes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-
-    </>
-  )
+    <Container>
+      <MyRoutes />
+    </Container>
+  );
 }
 
-export default App
+export default App;
+
+const Container = styled.div`
+  display: flex;
+  height: 997vh;
+  background: blue;
+  border-radius: 2rem;
+  @media screen and (min-width: 320px) and ( max-width: 1080px ) {
+      flex-direction: column;
+  }
+`;
--- Dasboard/src/pages/Home.jsx
+++ Dasboard/src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import styled  from "styled-components"
+
 export function Home(){
     return(
-        <h1>Home</h1>
+       <Container>
+        
+       </Container>
     )
 }
+
+//Estilos
+
+const Container = styled.div`
+  width:80%;
+  border-bottom-right-radius: 2rem;
+`;
