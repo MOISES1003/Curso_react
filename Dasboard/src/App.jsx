55db2d3c1f625a046c17e5f19618846ce6bb8b66
Moises <moiseswtf123@gmail.com>
2023-06-19 11:38:17 -0500

    agregando la card de ganancias

--- Dasboard/src/App.jsx
+++ Dasboard/src/App.jsx
@@ -12,12 +12,15 @@ function App() {
 
 export default App;
 
+
 const Container = styled.div`
   display: flex;
-  height: 997vh;
-  background: blue;
+  height: 97vh;
+  /* background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%); */
   border-radius: 2rem;
-  @media screen and (min-width: 320px) and ( max-width: 1080px ) {
-      flex-direction: column;
+  background: blue;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction:column;
+    
   }
 `;
--- Dasboard/src/pages/Home.jsx
+++ Dasboard/src/pages/Home.jsx
@@ -1,16 +1,68 @@
-import styled  from "styled-components"
+import styled from "styled-components";
+import { Ganancias } from "../components/Ganancias";
 
-export function Home(){
-    return(
-       <Container>
-        
-       </Container>
-    )
+export function Home() {
+  return (
+    <Container>
+      <SubContainer>
+        <SectionA>
+            <ColumnA1>
+            <Ganancias/>
+            </ColumnA1>
+        </SectionA>
+      </SubContainer>
+    </Container>
+  );
 }
 
 //Estilos
-
 const Container = styled.div`
-  width:80%;
+  width: 80%;
   border-bottom-right-radius: 2rem;
+  /* background:linear-gradient(to bottom right, white 0%, #e6e4ff 70%); */
+  border-top-right-radius: 2rem;
+  margin: 1rem 8rem 1rem 4rem;
+  background: red;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 1rem 0 0 0;
+  }
+`;
+const SubContainer = styled.div`
+  margin: 0.5rem 0;
+  height: 80%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  background: green;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: 100%;
+  }
+`;
+
+const SectionA = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 40%;
+  gap: 2rem;
+  width: 100%;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+    align-items: center;
+    height: max-content;
+  }
+`;
+const ColumnA1 = styled.div`
+  display: flex;
+  gap: 3rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    width: 100%;
+  }
 `;
