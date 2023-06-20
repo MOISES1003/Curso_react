3c2095f53ad6d7436b6a866ba2131fca30c25d4a
Moises <moiseswtf123@gmail.com>
2023-06-20 16:57:26 -0500

    terminando el componente para el logo

--- NftPage_react/src/components/Navigation.jsx
+++ NftPage_react/src/components/Navigation.jsx
@@ -1,21 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { Logo } from "./Logo";
 
-export function Navigation(){
-    return(
-        <Section>
-            hola
-        </Section>
-    )
-}   
-
+export function Navigation() {
+  return (
+    <Section>
+      <NavBar>
+        <Logo/>
+      </NavBar>
+    </Section>
+  );
+}
 
 /*---------------Estilos-----------------*/
 
 //estilos de la seccion
 const Section = styled.section`
-width: 100vw;
-/* height: 100vh; */
-background-color: ${(props)=>props.theme.body};
-
-
+  width: 100vw;
+  /* height: 100vh; */
+  background-color: ${(props) => props.theme.body};
+`;
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 85%;
+  height: ${(props) => props.theme.navHeight};
+  margin: 0 auto;
+  background: red;
 `;
