ae80511b19f97af99be070e4cdf5e10ea448fd20
Moises <moiseswtf123@gmail.com>
2023-06-25 17:30:28 -0500

    terminando las cartas del team

--- NftPage_react/src/components/sections/Team.jsx
+++ NftPage_react/src/components/sections/Team.jsx
@@ -12,13 +12,136 @@ import img9 from "../../assets/Nfts/bighead-8.svg";
 import img10 from "../../assets/Nfts/bighead-9.svg";
 // import ETH from "../../assets/icons8-ethereum-48.png";
 
-export function Team(){
-    return(
-        <Section id="team">
+const MemberComponent = ({ img, name = " ", position = " " }) => {
+  return (
+    <Item>
+      <ImageContainer>
+        <img width={500} height={400} src={img} alt={name} />
+      </ImageContainer>
+      <Name>{name}</Name>
+      <Position>{position}</Position>
+    </Item>
+  );
+};
 
-        </Section>
-    )
+export function Team() {
+  return (
+    <Section id="team">
+      <Title>Team</Title>
+      <Container>
+        <MemberComponent img={img1} name="Montenegro" position="Founder" />
+        <MemberComponent img={img2} name="Bilal" position="designer"/>
+        <MemberComponent img={img3} name="Salvador" position="Developer" />
+        <MemberComponent img={img4} name="Neus" position="designer" />
+        <MemberComponent img={img5} name="Luis " position="tester" />
+        <MemberComponent img={img6} name="Castells" position="Developer" />
+        <MemberComponent img={img7} name="Mauricio" position="tester" />
+        <MemberComponent img={img8} name="Saucedo" position="Developer" />
+        <MemberComponent img={img9} name="Carrillo" position="Developer" />
+        <MemberComponent img={img10} name="Raquel " position="designer" />
+      </Container>
+    </Section>
+  );
 }
 const Section = styled.section`
-    
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  overflow: hidden;
+`;
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media (max-width: 64em) {
+    width: 80%;
+  }
+  @media (max-width: 48em) {
+    width: 90%;
+    justify-content: center;
+  }
+`;
+
+//elementos de las tarjetas
+const Item = styled.div`
+  width: calc(20rem - 4vw);
+  padding: 1rem 0;
+  color: ${(props) => props.theme.body};
+  margin: 2rem 1rem;
+  position: relative;
+  z-index: 5;
+
+  backdrop-filter: blur(4px);
+
+  border: 2px solid ${(props) => props.theme.text};
+  border-radius: 20px;
+
+  &:hover {
+    img {
+      transform: translateY(-2rem) scale(1.2);
+    }
+  }
+
+  @media (max-width: 30em) {
+    width: 70vw;
+  }
+`;
+
+const ImageContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  background-color: ${(props) => props.theme.carouselColor};
+  border: 1px solid ${(props) => props.theme.text};
+  padding: 1rem;
+
+  border-radius: 20px;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: auto;
+    transition: all 0.3s ease;
+  }
+`;
+
+const Name = styled.h2`
+  font-size: ${(props) => props.theme.fontlg};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  color: ${(props) => props.theme.text};
+  margin-top: 1rem;
+`;
+
+const Position = styled.h2`
+  font-size: ${(props) => props.theme.fontmd};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.textRgba},0.9)`};
+  font-weight: 400;
 `;
