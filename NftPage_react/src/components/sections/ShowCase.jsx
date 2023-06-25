1a2bf1d2e3d566d36412482b3c3d5635f3717e6d
Moises <moiseswtf123@gmail.com>
2023-06-25 16:43:14 -0500

    Terminando la prte de showCase

--- NftPage_react/src/components/sections/ShowCase.jsx
+++ NftPage_react/src/components/sections/ShowCase.jsx
@@ -1,4 +1,4 @@
-import styled, {keyframes} from "styled-components"
+import styled, { keyframes } from "styled-components";
 import img1 from "../../assets/Nfts/bighead.svg";
 import img2 from "../../assets/Nfts/bighead-1.svg";
 import img3 from "../../assets/Nfts/bighead-2.svg";
@@ -10,25 +10,57 @@ import img8 from "../../assets/Nfts/bighead-7.svg";
 import img9 from "../../assets/Nfts/bighead-8.svg";
 import img10 from "../../assets/Nfts/bighead-9.svg";
 import ETH from "../../assets/icons8-ethereum-48.png";
+import { useRef } from "react";
 
-const NftItem=({img,number=0,price=0})=>{
-    return(
-        <ImgContainer>
-        <img src={img} alt=""  width={500} height={400}/>
-        </ImgContainer>
-    )
-}
+const NftItem = ({ img, number = 0, price = 0, passRef }) => {
+  let play = (e) => {
+    passRef.current.style.animationPlayState  = "running";
+  };
+  let pause = (e) => {
+    passRef.current.style.animationPlayState  = "paused";
+  };
+  return (
+    <ImgContainer onMouseOver={e=>pause(e)} onMouseOut={e=>play(e)}>
+      <img src={img} alt="" width={500} height={400} />
+      <Details>
+        <div>
+          <span>Nfts</span>
+          <br />
+          <h1>#{number}</h1>
+        </div>
+        <div>
+          <span>Price</span>
+          <Price>
+            <img src={ETH} alt="ETH" height={200} width={200} />
+            <h1>{Number(price).toFixed(1)}</h1>
+          </Price>
+        </div>
+      </Details>
+    </ImgContainer>
+  );
+};
 
-export function ShowCase(){
-    return(
-        <Section id="showcase">
-            <Row direction="none">
-                <NftItem img={img1} number={847} price={1.5} />
-                <NftItem img={img2} number={847} price={1.5} />
-                <NftItem img={img3} number={847} price={1.5} />
-            </Row>
-        </Section>
-    )
+export function ShowCase() {
+  const Row1Ref = useRef(null);
+  const Row2Ref = useRef(null);
+  return (
+    <Section id="showcase">
+      <Row direction="none" ref={Row1Ref}>
+        <NftItem img={img1} number={847} price={1.5} passRef={Row1Ref} />
+        <NftItem img={img2} number={847} price={1.5} passRef={Row1Ref} />
+        <NftItem img={img3} number={847} price={1.5} passRef={Row1Ref} />
+        <NftItem img={img4} number={847} price={1.5} passRef={Row1Ref} />
+        <NftItem img={img5} number={847} price={1.5} passRef={Row1Ref} />
+      </Row>
+      <Row direction="reverse" ref={Row2Ref}>
+        <NftItem img={img6} number={847} price={1.5} passRef={Row2Ref} />
+        <NftItem img={img7} number={847} price={1.5} passRef={Row2Ref} />
+        <NftItem img={img8} number={847} price={1.5} passRef={Row2Ref} />
+        <NftItem img={img9} number={847} price={1.5} passRef={Row2Ref} />
+        <NftItem img={img10} number={847} price={1.5} passRef={Row2Ref} />
+      </Row>
+    </Section>
+  );
 }
 
 //animaciones
@@ -39,50 +71,81 @@ const move = keyframes`
 //estilos
 const ImgContainer = styled.div`
   width: 15rem;
-  margin: 0  1rem;
-  background-color: ${(props)=>props.theme.body};
+  margin: 0 1rem;
+  background-color: ${(props) => props.theme.body};
   border-radius: 20px;
   cursor: pointer;
-  
+
   @media (max-width: 48em) {
     width: 10rem;
   }
   @media (max-width: 30em) {
     width: 10rem;
   }
-  img{
-    height:auto;
-    width:100%;
+  img {
+    height: auto;
+    width: 100%;
   }
 `;
 const Section = styled.section`
   min-height: 100vh;
   width: 100vw;
-  background-color: ${(props)=>props.theme.text};
+  background-color: ${(props) => props.theme.text};
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   position: relative;
   overflow: hidden;
-  &>*:first-child{
+  & > *:first-child {
     animation-duration: 20s;
     @media (max-width: 30em) {
-        animation-duration: 15s;
+      animation-duration: 15s;
     }
   }
-  &>*:last-child{
+  & > *:last-child {
     animation-duration: 15s;
     @media (max-width: 30em) {
-        animation-duration: 10s;
+      animation-duration: 10s;
     }
   }
 `;
 const Row = styled.div`
-  
   white-space: nowrap;
   box-sizing: content-box;
   margin: 2rem 0;
   display: flex;
-  animation:${move} linear infinite ${(props)=>props.direction}
+  animation: ${move} linear infinite ${(props) => props.direction};
+`;
+const Details = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.8rem 1rem;
+  background-color: ${(props) => props.theme.text};
+  border: 2px solid ${(props) => `rgba(${props.theme.bodyRgba},0.5)`};
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  span {
+    font-size: ${(props) => props.theme.fontsm};
+    color: ${(props) => `rgba(${props.theme.bodyRgba},0.5)`};
+    font-weight: 600;
+    line-height: 1.5rem;
+  }
+  h1 {
+    font-size: ${(props) => props.theme.fontmd};
+    color: ${(props) => props.theme.body};
+    font-weight: 600;
+    @media (max-width: 30em) {
+      font-size: ${(props) => props.theme.fontsm};
+    }
+  }
+`;
+const Price = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  img {
+    width: 1rem;
+    height: auto;
+  }
 `;
